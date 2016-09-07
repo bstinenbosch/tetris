1b13387b0014824413a29be84902a36d3479f89c
RobertLevenbach <robertlevenbach@gmail.com>
2016-09-07 11:59:11 +0200

    Update Grid.java

--- src/tetris/Grid.java
+++ src/tetris/Grid.java
@@ -4,6 +4,9 @@ public class Grid {
 	private static int[][] board = new int[10][24];
 
 	public void init(){
-		
+		for(int i=0; i< board[0].length; i++){
+			for(int j=0; j< board.length;j++){
+				System.out.print(".");
+			}System.out.println("");
 	}
 }
