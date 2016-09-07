881514428058fbe17dbc9bb036024404c40fc7b0
bstinenbosch <basstinenbosch@msn.com>
2016-09-07 12:13:01 +0200

    Grid verder

--- src/tetris/Grid.java
+++ src/tetris/Grid.java
@@ -3,7 +3,21 @@ package tetris;
 public class Grid {
 	private static int[][] board = new int[10][24];
 
-	public void init(){
-		
+	public static void init(){
+		for (int[] boardRow: board){
+			for (int i: boardRow){
+				boardRow[i] = 0;
+			}
+		}
+	}
+	
+	public static void drawBoard() {
+		String row = "";
+		for (int[] boardRow: board){
+			
+			for (int i: boardRow){
+				boardRow[i] = 0;
+			}
+		}
 	}
 }
