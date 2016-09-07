a39193b1625457885070f01ac165195dbffd3297
bstinenbosch <basstinenbosch@msn.com>
2016-09-07 11:52:33 +0200

    Grid class

--- src/tetris/Controller.java
+++ src/tetris/Controller.java
@@ -1,6 +1,6 @@
 package tetris;
 
-public class tetris {
+public class Controller {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/tetris/Grid.java
@@ -0,0 +1,9 @@
+package tetris;
+
+public class Grid {
+	private static int[][] board = new int[10][24];
+
+	public void init(){
+		
+	}
+}
