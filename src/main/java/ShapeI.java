a8c4e858668ea712f4ad1d7d192ecd6e5d4fb1fe
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-09-08 20:56:19 +0200

    Revert: test if automated testing works

--- src/main/java/ShapeI.java
+++ src/main/java/ShapeI.java
@@ -4,7 +4,7 @@ public class ShapeI implements IShape {
 
     private static int[][] shape0 =
             {
-                    {0, 1, 0, 0},
+                    {0, 0, 0, 0},
                     {1, 1, 1, 1},
                     {0, 0, 0, 0},
                     {0, 0, 0, 0}
