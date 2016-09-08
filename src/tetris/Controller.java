c53e08ce0f5981d57679787d9a88155e729a04f1
Karel van Doesburg <Karel van Doesburg>
2016-09-08 14:30:17 +0200

    nieuwe frame, ik moet zelf nog wel even kijken hoe het allemaal werk

--- src/tetris/Controller.java
+++ src/tetris/Controller.java
@@ -1,25 +1,60 @@
 package tetris;
 
-import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+import java.util.*;
+import java.lang.*;
 
-public class Controller {
+import javax.swing.JFrame;
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Custom Painting");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel panel = new JPanel();
-
-        panel.addKeyListener(new TetrisKeyListener()); //Hiermee laad je de KeyListener in!!
-
-        frame.add(panel);
-
-        frame.setSize(500, 500);
-        frame.setVisible(true);
-
-        //make sure the JPanel has the focus
-        panel.requestFocusInWindow();
-	}
-
-}
+public class Controller extends Canvas implements Runnable{
 	
+	public static final int WIDTH = 400, HEIGHT = 565;
+	
+	public static void main(String args[]){
+		final JFrame frame = new JFrame("Tetris");
+		frame.setSize(WIDTH,HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		Controller tm = new Controller();
+		frame.add(tm);
+		frame.setVisible(true);
+		tm.start();
+		
+		tm.addKeyListener(new TetrisKeyListener()); //Hiermee laad je de KeyListener in!!
+	}
+	
+	public void start(){
+		Thread t = new Thread(this);
+		t.setPriority(Thread.MAX_PRIORITY);
+		t.start();
+	}
+	
+	public void run(){
+		boolean running = true;
+		while(running){
+			update();
+			BufferStrategy buf = getBufferStrategy();
+			if(buf == null){
+				createBufferStrategy(3);
+				continue;
+			}
+			Graphics2D g = (Graphics2D) buf.getDrawGraphics();
+			render(g);
+			buf.show();
+		}
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void render(Graphics2D g){
+		g.setColor(Color.BLACK);
+		g.fillRect(0,0,WIDTH,HEIGHT);
+		g.setColor(Color.WHITE);
+		g.drawString("TETRIS", 170, 50);
+	}
+}
