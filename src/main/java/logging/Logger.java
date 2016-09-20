9860c4f7b17cd4c590a325bb6112ba6ccb985e16
Sebastiaan Vermeulen <mail@sebastiaanvermeulen.nl>
2016-09-20 17:13:23 +0200

    adapted testing to changes in rotation

--- src/main/java/logging/Logger.java
+++ src/main/java/logging/Logger.java
@@ -1,148 +1,156 @@
 package logging;
 
-import java.lang.StringBuilder;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
-public final class Logger{
-	private static boolean debug = false;
-	private static StringBuilder queue = new StringBuilder();
-	private static File file = new File("./logging/log.txt");
-	private static int logLength = 10000;
-	
-	public enum LogType {INFO, WARNING, ERROR};
-	
-	private Logger(){} //unreachable because static
-	
-	private static void writeToFile() throws IOException {
-		if (!file.exists()){
-			file.createNewFile();
-		}
-		FileWriter writer = new FileWriter(file, true);
-		writer.write(queue.toString());
-		writer.close();
-		capFileSize();
+public final class Logger {
+    private static boolean debug = false;
+    private static StringBuilder queue = new StringBuilder();
+    private static File file = new File("./logging/log.txt");
+    private static int logLength = 10000;
+
+    public enum LogType {
+	INFO, WARNING, ERROR
+    };
+
+    private Logger() {
+    } // unreachable because static
+
+    private static void writeToFile() throws IOException {
+	if (!file.exists()) {
+	    file.createNewFile();
 	}
-	
-	private static void capFileSize() throws IOException {
-		int fileLength = countLines();
-		if(fileLength>logLength){
-			String line;
-			File tempFile = File.createTempFile("TETRIS_LOG_", ".log");
-			BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-			try {
-				BufferedReader reader = new BufferedReader(new FileReader(file));
-				try {
-					skipLines(fileLength - logLength, reader);
-					while((line = reader.readLine()) != null){
-						writer.write(line);
-						writer.newLine();
-					}
-				} finally {
-					reader.close();
-				} 
-			} finally {
-				writer.close();
-			}
-			Files.move(tempFile.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-		}
-	}
-	
-	private static void skipLines(int lines, BufferedReader file) throws IOException{
-		for(int i=0;i<lines;i++){
-			file.readLine();
-		}
-	}
-	
-	private static int countLines() throws IOException {
-        char[] buffer = new char[1024];
-        int count = 0;
-        int readChars = 0;
-        boolean empty = true;
-	    BufferedReader reader = new BufferedReader(new FileReader(file));
+	FileWriter writer = new FileWriter(file, true);
+	writer.write(queue.toString());
+	writer.close();
+	capFileSize();
+    }
+
+    private static void capFileSize() throws IOException {
+	int fileLength = countLines();
+	if (fileLength > logLength) {
+	    String line;
+	    File tempFile = File.createTempFile("TETRIS_LOG_", ".log");
+	    BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
 	    try {
-	        while ((readChars = reader.read(buffer)) != -1) {
-	            empty = false;
-	            for (int i = 0; i < readChars; ++i) {
-	                if (buffer[i] == '\n') {
-	                    ++count;
-	                }
-	            }
-	        }
-	        return (count == 0 && !empty) ? 1 : count;
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		try {
+		    skipLines(fileLength - logLength, reader);
+		    while ((line = reader.readLine()) != null) {
+			writer.write(line);
+			writer.newLine();
+		    }
+		} finally {
+		    reader.close();
+		}
 	    } finally {
-	        reader.close();
+		writer.close();
+	    }
+	    Files.move(tempFile.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+	}
+    }
+
+    private static void skipLines(int lines, BufferedReader file) throws IOException {
+	for (int i = 0; i < lines; i++) {
+	    file.readLine();
+	}
+    }
+
+    private static int countLines() throws IOException {
+	char[] buffer = new char[1024];
+	int count = 0;
+	int readChars = 0;
+	boolean empty = true;
+	BufferedReader reader = new BufferedReader(new FileReader(file));
+	try {
+	    while ((readChars = reader.read(buffer)) != -1) {
+		empty = false;
+		for (int i = 0; i < readChars; ++i) {
+		    if (buffer[i] == '\n') {
+			++count;
+		    }
+		}
+	    }
+	    return (count == 0 && !empty) ? 1 : count;
+	} finally {
+	    reader.close();
+	}
+    }
+
+    /**
+     * Log lets you log a line in the log file, conditional on the debug mode
+     * being on.
+     * 
+     * @param sender
+     *            the object invoking the log statement
+     * @param logtype
+     *            the log type, for homogeneity constrained in the LogType enum
+     * @param message
+     *            the message that is logged
+     * @return true if the log was successfully registered, else false
+     */
+    public static boolean Log(Object sender, LogType logtype, String message) {
+	if (debug) {
+	    String msg = String.format("[%s] message from object %s: %s\n", logtype.toString(), sender.toString(),
+		    message);
+	    queue.append(msg);
+	    if (queue.length() > Math.min(logLength, 100)) {
+		try {
+		    writeToFile();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		    return false;
+		}
 	    }
 	}
-	
-	/**
-	 * Log lets you log a line in the log file, conditional on the debug mode being on.
-	 * @param sender the object invoking the log statement
-	 * @param logtype the log type, for homogeneity constrained in the LogType enum
-	 * @param message the message that is logged
-	 * @return true if the log was successfully registered, else false
-	 */
-	public static boolean Log(Object sender, LogType logtype, String message){
-		if(debug){
-			String msg = String.format("[%s] message from object %s: %s\n", logtype.toString(), sender.toString(), message);
-			queue.append(msg);
-			if(queue.length()>Math.min(logLength, 100)){
-				try {
-					writeToFile();
-				} catch (IOException e) {
-					e.printStackTrace();
-					return false;
-				}
-			}
-		}
-		return true;
+	return true;
+    }
+
+    /**
+     * clearLog truncates the log, in case you accidentally logged a nude
+     * picture or something.
+     */
+    public static void clearLog() {
+	try {
+	    Files.deleteIfExists(file.toPath());
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
-	
-	/**
-	 * clearLog truncates the log, in case you accidentally logged a nude picture or something.
-	 */
-	public static void clearLog(){
-		try {
-			Files.deleteIfExists(file.toPath());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    }
+
+    /* getters / setters */
+    public static int getLogLength() {
+	return logLength;
+    }
+
+    public static void setLogLength(int length) {
+	logLength = length;
+    }
+
+    public static void setLogDir(String path) {
+	file = new File(path);
+    }
+
+    public static String getLogDir() {
+	return file.toString();
+    }
+
+    public static void setDebugOn() {
+	debug = true;
+    }
+
+    public static void setDebugOff() {
+	try {
+	    writeToFile();
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
-	
-	/* getters / setters */
-	public static int getLogLength(){
-		return logLength;
-	}
-	
-	public static void setLogLength(int length){
-		logLength = length;
-	}
-	
-	public static void setLogDir(String path){
-		file = new File(path);
-	}
-	
-	
-	public static String getLogDir(){
-		return file.toString();
-	}
-	
-	public static void setDebugOn(){
-		debug = true;
-	}
-	
-	public static void setDebugOff(){
-		try {
-			writeToFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		debug = false;
-	}
+	debug = false;
+    }
 }
--- src/test/java/tetris/DummyShape.java
+++ src/test/java/tetris/DummyShape.java
@@ -4,8 +4,8 @@ import tetris.tetromino.AbstractTetromino;
 
 public class DummyShape extends AbstractTetromino {
 
-    private static final Coordinate[] minos = new Coordinate[] { new Coordinate(-1, 0), new Coordinate(0, 0),
-	    new Coordinate(0, 1), new Coordinate(1, 1) };
+    private static final Coordinate[] minos = new Coordinate[] { new Coordinate(0, 0), new Coordinate(0, 1),
+	    new Coordinate(0, 2), new Coordinate(1, 1) };
 
     public DummyShape(Coordinate position) {
 	super(position, minos);
