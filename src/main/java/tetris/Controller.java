23631b44c46ccad2510ee603afe593e16068aefe
Sebastiaan Vermeulen <mail@sebastiaanvermeulen.nl>
2016-09-26 16:58:31 +0200

    implemented nintendo point assignment system

--- src/main/java/tetris/Controller.java
+++ src/main/java/tetris/Controller.java
@@ -12,24 +12,35 @@ public class Controller {
 
     private class Score extends Observable {
         private int score = 0;
+        private int level = 0;
+        private int[] pointsPerRow = { 0, 40, 50, 100, 300 };
 
         /**
          * resets the score to 0.
          */
         public void reset() {
             score = 0;
+            level = 0;
         }
 
         /**
          * increases the score by add.
          * 
-         * @param add
+         * @param rowsCleared
          *            add add points to the score
          */
-        public void add(int add) {
-            score += add;
+        public void add(int rowsCleared) {
+            score += rowsCleared * pointsPerRow[rowsCleared];
+            level += rowsCleared;
             setChanged();
             notifyObservers(score);
+        }
+
+        /**
+         * @return the current level that has been reached.
+         */
+        public int getLevel() {
+            return level / 10;
         }
     }
 
