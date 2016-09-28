2a2e22108a57841a8a286edba9a53dcdf1413dc3
Sebastiaan Vermeulen <mail@sebastiaanvermeulen.nl>
2016-09-28 09:20:57 +0200

    game screen separate class

--- src/main/java/tetris/View.java
+++ src/main/java/tetris/View.java
@@ -1,17 +1,10 @@
 package tetris;
 
 import javafx.application.Application;
-import javafx.geometry.Orientation;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -32,8 +25,7 @@ public class View extends Application {
     private Controller controller;
     private Stage primaryStage;
     private GraphicsContext board;
-    private Button settingsButton;
-//    private GameSettingsPanel gameSettingsPanel = new GameSettingsPanel();
+    // private GameSettingsPanel gameSettingsPanel = new GameSettingsPanel();
     private ScreenController screenController = new ScreenController();
     private MainScreen mainScreen = new MainScreen();
     private SettingsScreen settingsScreen = new SettingsScreen();
@@ -49,8 +41,8 @@ public class View extends Application {
         controller = new Controller(this);
         this.primaryStage = primaryStage;
         gotoLauncher();
-//        primaryStage.setTitle("Tetris");
-//        primaryStage.show();
+        // primaryStage.setTitle("Tetris");
+        // primaryStage.show();
     }
 
     /**
@@ -93,51 +85,8 @@ public class View extends Application {
      * fires up the game controller.
      */
     public void gotoGameScreen() {
-        Pane leftPane = setUpLeftPaneGameScreen();
-        GridPane rightPane = setUpRightPaneGameScreen();
-        GridPane rootGameScreen = setUpRootPaneGameScreen(leftPane, rightPane);
-
-        Scene gameScreen = new Scene(rootGameScreen);
-        primaryStage.setScene(gameScreen);
-        gameScreen.getRoot().requestFocus();
-    }
-
-    private Pane setUpLeftPaneGameScreen() {
-        Canvas canvas = new Canvas(BLOCK_SIZE * BOARD_WIDTH, BLOCK_SIZE * BOARD_HEIGHT);
-        board = canvas.getGraphicsContext2D();
-        Pane leftPane = new Pane();
-        leftPane.getChildren().add(canvas);
-        leftPane.setStyle("-fx-background-color: grey");
-        return leftPane;
-    }
-
-    private GridPane setUpRootPaneGameScreen(Pane leftPane, GridPane rightPane) {
-        GridPane rootGameScreen = new GridPane();
-        rootGameScreen.setHgap(10);
-        GridPane.setConstraints(leftPane, 0, 0);
-        GridPane.setConstraints(rightPane, 1, 0);
-        rootGameScreen.getChildren().addAll(leftPane, rightPane);
-        rootGameScreen.setOnKeyPressed(event -> controller.handleKeyEvent(event));
-        return rootGameScreen;
-    }
-
-    private GridPane setUpRightPaneGameScreen() {
-        Button exitButton = new Button("exit");
-        Button restartButton = new Button("restart");
-        hookGameScreenEvents(exitButton, restartButton);
-
-        GridPane rightPane = new GridPane();
-        rightPane.setVgap(10);
-        GridPane.setConstraints(exitButton, 0, 0);
-        GridPane.setConstraints(restartButton, 0, 1);
-        rightPane.getChildren().addAll(exitButton, restartButton);
-        rightPane.setStyle("-fx-background-color: grey");
-        return rightPane;
-    }
-
-    private void hookGameScreenEvents(Button exitButton, Button restartButton) {
-        exitButton.setOnAction(event -> controller.stop());
-        restartButton.setOnAction(event -> controller.restartGame());
+        primaryStage.setScene(new Scene(new GameView()));
+        primaryStage.getScene().getRoot().requestFocus();
     }
 
     /**
