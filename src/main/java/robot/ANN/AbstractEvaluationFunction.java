2966a91fbd72ef9bd1bf18bcab1dff085c470650
Sebastiaan Vermeulen <mail@sebastiaanvermeulen.nl>
2016-10-09 14:09:41 +0200

    Start of NEAT robot: ANN framework mostly implemented. Largest part of the genetic algorithm implemented. NO way to hook it to the robotcontroller though

---
+++ src/main/java/robot/ANN/AbstractEvaluationFunction.java
@@ -0,0 +1,26 @@
+package robot.ANN;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public abstract class AbstractEvaluationFunction {
+    protected HashMap<AbstractNeuron, Double> weights;
+
+    public AbstractEvaluationFunction() {
+        weights = new HashMap<AbstractNeuron, Double>();
+    }
+
+    /**
+     * evaluate determines what charge is passed to the Neuron on the other
+     * side.
+     * 
+     * @param neurons
+     *            the neurons whose charge must be evaluated.
+     * @return the output charge. Should at all times be in [0,1].
+     */
+    public abstract double evaluate(ArrayList<AbstractNeuron> neurons);
+
+    public final void update(AbstractNeuron neuron, double weight) {
+        weights.put(neuron, weight);
+    }
+}
