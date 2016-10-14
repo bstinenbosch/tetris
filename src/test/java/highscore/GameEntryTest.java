1f711ec72391292ef5243a3cb6272709cd9eed77
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-10-14 13:00:56 +0200

    Remove unnecessary tests

--- src/test/java/highscore/GameEntryTest.java
+++ src/test/java/highscore/GameEntryTest.java
@@ -13,20 +13,6 @@ public class GameEntryTest {
     }
 
     @Test
-    public void toStringSingleConstructorTest() {
-        GameEntry entry = new GameEntry(500);
-        assertEquals("(, 500)", entry.toString());
-    }
-
-    @Test
-    public void setNameTest() {
-        GameEntry entry = new GameEntry(500);
-        assertEquals("(, 500)", entry.toString());
-        entry.setName("Bas");
-        assertEquals("(Bas, 500)", entry.toString());
-    }
-
-    @Test
     public void getNameTest() {
         GameEntry entry = new GameEntry("Bas", 500);
         assertEquals("Bas", entry.getName());
