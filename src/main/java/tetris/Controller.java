33cd0e0a5074fa0f1eb908ced50297fa778d350e
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-10-14 13:06:12 +0200

    Go to directly to highscore screen if no highscore was set

--- src/main/java/tetris/Controller.java
+++ src/main/java/tetris/Controller.java
@@ -141,6 +141,8 @@ public class Controller {
                     ui.gotoPromptNameScreen();
                 }
             });
+        } else {
+            ui.gotoHighscoreScreen();
         }
     }
 
