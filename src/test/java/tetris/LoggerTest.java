089297ea3cdaa6e23805e69b6abd036cdeefae91
Sebastiaan Vermeulen <mail@sebastiaanvermeulen.nl>
2016-10-15 11:08:34 +0200

    possible solution to travis' loggertest bitchyness

--- src/test/java/tetris/LoggerTest.java
+++ src/test/java/tetris/LoggerTest.java
@@ -1,16 +1,11 @@
 package tetris;
 
-import static com.jayway.awaitility.Awaitility.with;
-import static com.jayway.awaitility.Duration.*;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.concurrent.Callable;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -19,50 +14,57 @@ import logging.Logger;
 
 public class LoggerTest {
 
+    /**
+     * By putting all tests sync'ed in one method the tests can't be executed in
+     * different threads, since they are not thread-safe.
+     * 
+     * @throws Exception
+     */
     @Test
-    public void test_logcreate() throws Exception {
+    public void testAll() throws Exception {
+        test_capLog();
+        test_debugMode();
+        test_getters();
+        test_logcreate();
+        test_logdelete();
+    }
+
+    public synchronized void test_logcreate() throws Exception {
         String testloc = "test.log";
         Logger.setLogDir(testloc);
         // Logger.clearLog();
         Logger.setDebugOn();
         Logger.log(this, Logger.LogType.ERROR, "test 1");
         Logger.setDebugOff();
-
-        asyncWaitForFileCreation(testloc);
         assertTrue(new File(testloc).exists());
     }
 
-    @Test
-    public void test_logdelete() throws Exception {
+    public synchronized void test_logdelete() throws Exception {
         String testloc = "test.log";
         Logger.setLogDir(testloc);
         Logger.setDebugOn();
         Logger.log(this, Logger.LogType.ERROR, "test 1");
 
-        asyncWaitForFileCreation(testloc);
-        assertTrue(new File(testloc).exists());
-
         Logger.setDebugOff();
+        assertTrue(new File(testloc).exists());
+        Logger.setDebugOn();
         Logger.clearLog();
-
-        asyncWaitForFileRemoval(testloc);
+        Logger.setDebugOff();
         assertFalse(new File(testloc).exists());
     }
 
-    @Test
-    public void test_debugMode() throws Exception {
+    public synchronized void test_debugMode() throws Exception {
         String testloc = "test.log";
         Logger.setLogDir(testloc);
         Logger.setDebugOff();
         Logger.clearLog();
         Logger.log(this, Logger.LogType.ERROR, "test 1");
-
-        asyncWaitForFileRemoval(testloc);
+        Logger.setDebugOff();
+        // asyncWaitForFileRemoval(testloc);
         assertFalse(new File(testloc).exists());
     }
 
-    @Test
-    public void test_capLog() throws Exception {
+    public synchronized void test_capLog() throws Exception {
         String testloc = "test.log";
         Logger.setLogDir(testloc);
         Logger.setLogLength(10);
@@ -78,7 +80,7 @@ public class LoggerTest {
 
         File testlocFile = new File(testloc);
 
-        asyncWaitForFileCreation(testloc);
+        // asyncWaitForFileCreation(testloc);
         assertTrue(testlocFile.exists());
 
         int count = 0;
@@ -93,35 +95,8 @@ public class LoggerTest {
         assertEquals(10, count);
     }
 
-    @Test
-    public void test_getters() throws ClassCastException {
+    public synchronized void test_getters() throws ClassCastException {
         assertTrue(Logger.getLogDir() instanceof String);
         assertTrue(Logger.getLogLength() == Logger.getLogLength());
-    }
-
-    private void asyncWaitForFileCreation(String testloc) throws Exception {
-        with().pollDelay(ONE_HUNDRED_MILLISECONDS).and().with()
-            .pollInterval(TWO_HUNDRED_MILLISECONDS).and().with().timeout(ONE_MINUTE)
-            .await("file creation").until(fileIsCreatedOnDisk(testloc), equalTo(true));
-    }
-
-    private void asyncWaitForFileRemoval(String testloc) throws Exception {
-        with().pollDelay(ONE_HUNDRED_MILLISECONDS).and().with()
-            .pollInterval(TWO_HUNDRED_MILLISECONDS).and().with().timeout(ONE_MINUTE)
-            .await("file removal").until(fileIsRemovedFromDisk(testloc), equalTo(true));
-    }
-
-    private Callable<Boolean> fileIsCreatedOnDisk(final String filename) {
-        return () -> {
-            File file = new File(filename);
-            return file.exists();
-        };
-    }
-
-    private Callable<Boolean> fileIsRemovedFromDisk(final String filename) {
-        return () -> {
-            File file = new File(filename);
-            return !file.exists();
-        };
     }
 }
