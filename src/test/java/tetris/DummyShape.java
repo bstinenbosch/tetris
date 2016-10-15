63d0d13f545994d8b0c4c6542b21b1508fb2b74b
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-10-16 01:02:21 +0200

    Split test into different cases and use a dummy shape for testing Grid class

--- src/test/java/tetris/DummyShape.java
+++ src/test/java/tetris/DummyShape.java
@@ -5,10 +5,14 @@ import tetris.tetromino.AbstractTetromino;
 public class DummyShape extends AbstractTetromino {
 
     private static final Coordinate[] minos = new Coordinate[] { new Coordinate(0, 0),
-        new Coordinate(0, 1), new Coordinate(0, 2), new Coordinate(1, 1) };
+        new Coordinate(0, 1), new Coordinate(1, 0), new Coordinate(1, 1) };
 
     private static int color = 1;
 
+    /**
+     * Creates a DummyShape that is similar in shape as the O-shaped Tetromino
+     * @param position coordinate where shape is registered in the grid
+     */
     public DummyShape(Coordinate position) {
         super(position, minos, color);
     }
