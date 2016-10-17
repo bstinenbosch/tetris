d2886bd71e73a9cadecb58f403b970598511cf52
Sebastiaan Vermeulen <mail@sebastiaanvermeulen.nl>
2016-10-17 13:13:08 +0200

    fixed illegalthreadstateexception: ontick event is run via platform.runlater and ticktest is split up in multiple tests that also test more cases.

--- src/test/java/tetris/TickTest.java
+++ src/test/java/tetris/TickTest.java
@@ -7,25 +7,51 @@ public class TickTest extends Thread {
     boolean trueReported = false;
 
     @Test
-    public void test_logcreate() throws InterruptedException {
+    public void test_fire_ontick_event_paused() throws InterruptedException {
+        trueReported = false;
         Tick tick = new Tick(event -> reportTrue());
-        tick.setTime(500);
-        Assert.assertEquals(tick.getTime(), 500);
-
+        tick.setTime(100);
         tick.pause();
         tick.start();
-        sleep(550);
+        sleep(1550);
         Assert.assertFalse(trueReported);
+        tick.requestStop();
+    }
+
+    @Test
+    public void test_fire_ontick_event_unpaused() throws InterruptedException {
+        trueReported = false;
+        Tick tick = new Tick(event -> reportTrue());
+        tick.setTime(100);
         tick.unpause();
-        sleep(550);
+        tick.start();
+        sleep(1550);
         Assert.assertTrue(trueReported);
         tick.requestStop();
+
+    }
+
+    @Test
+    public void test_fire_ontick_event_stoped() throws InterruptedException {
+        Tick tick = new Tick(event -> reportTrue());
+        tick.setTime(100);
+        tick.start();
+        tick.unpause();
+        tick.requestStop();
+        sleep(1550);
         trueReported = false;
-        sleep(550);
+        sleep(1550);
         Assert.assertFalse(trueReported);
     }
 
     private void reportTrue() {
         trueReported = true;
+    }
+
+    @Test
+    public void test_setters_getters() {
+        Tick tick = new Tick(event -> reportTrue());
+        tick.setTime(500);
+        Assert.assertEquals(tick.getTime(), 500);
     }
 }
