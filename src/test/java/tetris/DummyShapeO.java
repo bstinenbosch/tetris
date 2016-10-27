e64bffbb71f0818ccb171008c32f160f8adb6946
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-10-27 21:13:45 +0200

    Fix shapes

--- src/test/java/tetris/DummyShapeO.java
+++ src/test/java/tetris/DummyShapeO.java
@@ -11,10 +11,10 @@ public class DummyShapeO extends AbstractShape {
     private static final CoordinateSet MINOS = new CoordinateSet(
             new ArrayList<Coordinate>() {
                 {
-                    add(new Coordinate(-1, 0));
+                    add(new Coordinate(0, 1));
                     add(new Coordinate(0, 0));
                     add(new Coordinate(1, 0));
-                    add(new Coordinate(1, -1));
+                    add(new Coordinate(1, 1));
                 }
             });
 
