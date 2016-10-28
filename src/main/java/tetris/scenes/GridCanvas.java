091a0e9197d86ed68368248bbe948e37b6f3fbed
Pascal Lubbers <pascallubbers8@hotmail.com>
2016-10-28 15:56:54 +0200

    Tiny little fix that prevents the drop shadow to overlap the falling shape

--- src/main/java/tetris/scenes/GridCanvas.java
+++ src/main/java/tetris/scenes/GridCanvas.java
@@ -20,8 +20,8 @@ public class GridCanvas extends Canvas {
     public void redraw() {
         clearBoard();
         drawGrid();
-        drawTetromino();
         drawTetrominoShadow();
+        drawTetromino();
     }
 
     private void drawTetrominoShadow() {
